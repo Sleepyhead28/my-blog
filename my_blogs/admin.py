59c61664dc41ec78b9b7a42205b85149786e5ecc
SZ <15601134977@163.com>
2018-08-22 15:03:31 +0800

    创建目录
    为blog创建了三级目录：category-topic-entry

--- my_blogs/admin.py
+++ my_blogs/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Category, Topic, Entry
+
+admin.site.register(Category)
+admin.site.register(Topic)
+admin.site.register(Entry)
--- my_blogs/models.py
+++ my_blogs/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    """个人博客的大类"""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        """返回模型的字符串表示"""
+        return self.text
+
+class Topic(models.Model):
+    """大类下每篇文章的标题"""
+    topic = models.ForeignKey(Category, on_delete=models.CASCADE)
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """返回模型的字符串表示"""
+        return self.text
+
+class Entry(models.Model):
+    """博客的正文"""
+    entry = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """返回模型的字符串表示"""
+        return self.text[:50] + "..."
+
+
+
